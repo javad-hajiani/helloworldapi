b703d6984ffecf22abdc496bdb74669ba79068f3
Javad Hajiani <j.hajiani@digikala.com>
2019-05-13 21:51:15 +0430

    formatting code

--- src/main/java/com/revolut/devops/challenge/Main.java
+++ src/main/java/com/revolut/devops/challenge/Main.java
@@ -1,23 +1,9 @@
 package com.revolut.devops.challenge;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.revolut.devops.challenge.entity.User;
 import com.revolut.devops.challenge.exceptions.InvalidBirthdateException;
 import com.revolut.devops.challenge.exceptions.InvalidUsernameException;
-import spark.Request;
-import spark.Response;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import static spark.Spark.get;
 import static spark.Spark.put;
@@ -31,7 +17,7 @@ public class Main {
             try {
                 String responseInString = mainServiceHub.putUserInfo(req.params("username"), req.body());
                 res.status(204);
-                return  responseInString;
+                return responseInString;
             } catch (InvalidUsernameException e) {
                 res.status(400);
                 e.printStackTrace();
@@ -47,17 +33,19 @@ public class Main {
             }
         });
         get("/hello/:username", (req, res) -> {
-           try{
-               res.status(200);
-               return  mainServiceHub.getUserInfo(req.params("username"));
-           } catch (IOException e){
+            try {
+                res.status(200);
+                return mainServiceHub.getUserInfo(req.params("username"));
+            } catch (IOException e) {
                 res.status(404);
-                return  e.getMessage()+">>> no such user exists. if you are sure of the user existence, please check whether USER_DATA_DIR has a value or not.";
-           }
+                return e.getMessage() + ">>> no such user exists. if you are sure of the user existence, please check whether USER_DATA_DIR has a value or not.";
+            }
         });
-        get("/*", (req, res) -> { res.status(200);  return  mainServiceHub.getInstruction();});
+        get("/*", (req, res) -> {
+            res.status(200);
+            return mainServiceHub.getInstruction();
+        });
     }
-
 
 
 }
